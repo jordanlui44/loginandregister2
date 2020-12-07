4b704cd235d54f25d58c9581792aee8eea863201
jordanlui44 <59802861+jordanlui44@users.noreply.github.com>
2020-12-06 23:46:51 -0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if(!isset($_SESSION['UserName'])){
     header('Location: login.php');
 }
 
-if(isset($_GET['logout'])){}
+if(isset($_GET['logout'])){
     session_destroy();
     unset($_SESSION['UserName']);
     header('Location: login.php');
